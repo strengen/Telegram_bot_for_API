0cf27f9aafede822858dfa91d25fedc0a5307011
v.cherkas <cherkass800322@gmail.com>
2025-12-26 19:55:09 +0100

    Main fixes and updates: - Fixed issues with converting process(scenario was breaking while converting) - Added navigation if entered text is undefined - Added checking if user exists on each step of scenario - Added possibility to delete a current user Small fixes: - Comfortable view of all available currencies - Comfortable view of registry date - Small lang fixes

--- currency_api.py
+++ currency_api.py
@@ -1,4 +1,4 @@
-from typing import Set, Dict
+from typing import List, Dict
 import requests
 
 from config import API_KEY
@@ -8,10 +8,11 @@ from currencies import CURRENCIES
 
 BASE_URL = 'https://www.amdoren.com/api/currency.php'
 
-def get_currencies() -> Set[str]:
-    """List of supported currencies"""
-    responce = set(CURRENCIES.keys())
-    return responce
+def get_currencies() -> List[str]:
+    return [
+        '{code}: {name}'.format(code=currency_code, name=currency_name)
+        for currency_code, currency_name in CURRENCIES.items()
+    ]
 
 def convert_currency(convert_from: str, convert_to: str, amount: float) -> Dict:
     """API request"""
